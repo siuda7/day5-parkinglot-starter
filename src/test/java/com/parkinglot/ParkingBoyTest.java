e6d5b41700ca28be895bbeb7eafa28a86c2a378b
Daniel Siu <daniel.siu@oocl.com>
2024-11-22 15:32:26 +0800

    [ParkingLot][Story3] feat: Case 3, return 2 correct cars given 2 ticket

--- src/test/java/com/parkinglot/ParkingBoyTest.java
+++ src/test/java/com/parkinglot/ParkingBoyTest.java
@@ -41,4 +41,28 @@ public class ParkingBoyTest {
 
     }
 
+
+    @Test
+    void should_return_2_cars_when_fetch_given_parking_lot_parking_boy_2_parked_cars_and_2_tickets() {
+
+        //Given
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+
+        Car car1 = new Car();
+        Ticket ticket1 = parkingBoy.park(car1);
+        Car car2 = new Car();
+        Ticket ticket2 = parkingBoy.park(car2);
+
+        //When
+        Car fetchCar1 = parkingBoy.fetch(ticket1);
+        Car fetchCar2 = parkingBoy.fetch(ticket2);
+
+        //Then
+        assertEquals(car1, fetchCar1);
+        assertEquals(car2, fetchCar2);
+
+    }
+
+
 }
